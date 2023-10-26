521e3633be1df282410e3309f2d91a4f099ac8a9
Louie <louie@Zipcoders-MacBook-Pro-3.local>
2023-10-26 15:32:41 -0400

    da lab is completed

--- src/main/java/LogginLab.java
+++ src/main/java/LogginLab.java
@@ -35,7 +35,14 @@ public class LogginLab {
             return false;
     }
 
+    public boolean thresholdReached(Integer limit) {
+        if (limit > this.threshold)
+            return true;
+        else
+            return false;
+    }
     // Write a method called thresholdReached, returns true if argument 'limit' is over the threshold.
     // use thresholdExceeds for a pattern.
     // Write a test for the method in the Test class.
+
 }
--- src/test/java/LogginLabTest.java
+++ src/test/java/LogginLabTest.java
@@ -23,11 +23,31 @@ public class LogginLabTest {
 
         for (Integer i = 1; i <= finalLimit; i++) {
             if (lab.thresholdExceeds(i)) {
-                logger.log(Level.INFO, "Threshold not reached! It is "+i);
+                logger.log(Level.INFO, "Threshold not exceed. Only "+ (finalLimit - i) +" to go!");
                 assertTrue(lab.thresholdExceeds(i));
             } else {
                 logger.log(Level.INFO, "Threshold finally reached!");
                 assertFalse(lab.thresholdExceeds(i));
+            }
+        }
+    }
+
+    @org.junit.Test
+    public void thresholdReached() {
+        Integer limit = 5;
+
+        LogginLab lab = new LogginLab();
+        lab.setThreshold(limit);
+
+        for (Integer i = 1; i <= limit + 1; i++) {
+            if (lab.thresholdReached(i)) {
+                logger.log(Level.INFO, "Threshold finally exceeded!");
+                assertTrue(lab.thresholdReached(i));
+            } else if (i == limit) {
+                logger.log(Level.INFO, "Threshold met.");
+            } else {
+                logger.log(Level.INFO, "Threshold not reached! Only "+ (limit - i) +" to go!");
+                assertFalse(lab.thresholdReached(i));
             }
         }
     }
